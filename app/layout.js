9ef91d73f6c2e8f631b3748fde99d76b19b0d121
sgledhill1695 <samgledhill1695@hotmail.com>
2024-02-17 10:44:40 +0000

    Vercel analytics added

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,8 @@ export default function RootLayout({ children }) {
 
                 {children}
 
+                <Analytics />
+                
               </div>
 
             </NotificationContextProvider>
